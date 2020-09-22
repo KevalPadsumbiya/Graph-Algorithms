8a4c5b783a4d60d177ecc9561ebc7f5db16ae18b
Keval Padsumbiya <68999502+KevalPadsumbiya@users.noreply.github.com>
2020-09-22 15:12:41 +0530

    Add files via upload

---
+++ is_bst_hard.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+#define lli long long int
+#define mod 1000000007
+#define pb push_back
+#define mk make_pair
+#define fastio ios_base::sync_with_stdio(false); cout.tie(NULL);
+using namespace std;
+
+struct BST 
+{
+    int key,leftchild,rightchild;
+    BST()
+	{
+		key=0;
+		leftchild=-1;
+		rightchild=-1;
+	}
+    BST(int key1,int leftchild1,int rightchild1)
+	{
+		key=key1;
+		leftchild=leftchild1;
+		rightchild=rightchild1; 
+	}
+};
+
+vector<BST> bst;
+vector<int> v,v1;
+
+void inOrder(int root) 
+{
+    if(root==-1) 
+		return;
+    inOrder(bst[root].leftchild);
+    v.pb(bst[root].key);
+    v1.pb(root);
+    inOrder(bst[root].rightchild);
+}
+
+int main() 
+{
+	fastio;
+    int n,i,key, leftchild, rightchild,f=0;
+
+    cin>>n;
+
+    for (i=0;i<n;i++) 
+        cin>>key>>leftchild>>rightchild, bst.pb(BST(key, leftchild, rightchild));
+
+    if(bst.size()>1)
+	{
+		inOrder(0);
+        for (i=1;i<v.size();i++) 
+            if(v[i]<v[i-1] || (v[i]==v[i-1] && bst[v1[i]].leftchild==v1[i-1])) 		 // if equal then check for left child because duplicate in right 
+            	f=1;																 // child is allowed	
+    }
+
+    if(f) 
+        cout<<"INCORRECT";
+    else
+        cout<<"CORRECT";
+    return 0;
+}
